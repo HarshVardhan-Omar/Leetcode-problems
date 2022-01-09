031b280bf8fac0e288ab48663177a37a7cbe0692
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-01-09 15:58:22 +0530

    Time: 5 ms (54.82%), Space: 42.2 MB (22.62%) - LeetHub

---
+++ 51-n-queens/51-n-queens.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board=new char[n][n];
+        for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                        board[i][j]='.';
+                }
+        }
+        List<List<String>> ans=new ArrayList<>();
+        solve(0,board,ans,n);
+         return ans;
+    }
+        private void solve(int col,char[][] board,List<List<String>> ans,int n){
+                if(col==n){
+                        ans.add(construct(board));
+                        return;
+                }
+                for(int row=0;row<n;row++){
+                        if(isSafe(row,col,board,n)){
+                                board[row][col]='Q';
+                                solve(col+1,board,ans,n);
+                                board[row][col]='.';
+                        }
+                }
+        }
+        private boolean isSafe(int row,int col,char[][] board,int n){
+                int duprow=row;
+                int dupcol=col;
+                while(row>=0&&col>=0){
+                        if(board[row][col]=='Q')
+                                return false;
+                        row--;
+                        col--;
+                }
+                row=duprow;
+                col=dupcol;
+                while(col>=0){
+                        if(board[row][col]=='Q')
+                                return false;
+                        col--;
+                }
+                col=dupcol;
+                while(row<n&&col>=0){
+                        if(board[row][col]=='Q')
+                                return false;
+                        row++;
+                        col--;
+                }
+                return true;
+        }
+        private List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for(int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
