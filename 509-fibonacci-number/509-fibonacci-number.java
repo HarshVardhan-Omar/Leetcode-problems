8879537deb90210acc2cdf882182b83f360d3931
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-01-26 22:53:04 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (20.01%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -5,7 +5,16 @@ class Solution {
             if(n==1)
                     return 1;
             
-            return fib(n-1)+fib(n-2);
+            int a=0;int b=1;
+            int c=a+b;
+            for(int i=3;i<=n;i++){
+                    // System.out.println(a+""+b+""+c);
+                    int temp=b;
+                    b=c;
+                    a=temp;
+                    c=a+b;
+            }
+            return c;
         
     }
 }
