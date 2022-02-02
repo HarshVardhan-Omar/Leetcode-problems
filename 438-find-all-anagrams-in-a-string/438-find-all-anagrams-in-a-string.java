654ce14695a1d9550b8324936d662289134693eb
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-02-02 12:00:37 +0530

    Time: 7 ms (86.35%), Space: 43.1 MB (54.13%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -19,24 +19,19 @@ class Solution {
             int[] count2=new int[26];
             int start=0;
             while(i<windowsize&&i<s.length()){
-                    // start=i;
                     int index=s.charAt(i)-'a';
                     count2[index]++;
                     i++;
             }
             if(checkequal(count1,count2))
                     ans.add(start);
-            // System.out.println(i);
             start=1;
             while(i<s.length()){
-                    // int startindex=i;
                     int index=s.charAt(i)-'a';
                     count2[index]++;
                     char oldchar=s.charAt(i-windowsize);
                     index=oldchar-'a';
                     count2[index]--;
-                    // System.out.println(Arrays.toString(count2));
-                    
                     if(checkequal(count1,count2))
                             ans.add(start);
                     i++;
