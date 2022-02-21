f79843c189d77b8d1e35002e49a06d35be98048c
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-02-21 19:58:04 +0530

    Time: 47 ms (5.20%), Space: 71.2 MB (35.52%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n=nums.length;
+            HashMap<Integer,Integer> mp=new HashMap<>();
+            for(int i=0;i<n;i++){
+                    mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            }
+            for(int i:mp.keySet()){
+                    if(mp.get(i)>=2)
+                            return true;
+            }
+            return false;
+                    
+    }
+}
