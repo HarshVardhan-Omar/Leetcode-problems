e664260d23fc9c39f760ab55b86d61711c8d5940
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-02-25 14:29:53 +0530

    Time: 24 ms (5.41%), Space: 43.2 MB (6.00%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -0,0 +1,95 @@
+class Solution {
+    private String removeleadingzero(String s){
+            int i=0;
+            while(s.charAt(i)=='0'&&s.length()>1){
+                  i++;
+                  if(i==s.length()){
+                          break;
+                  }
+            }
+            s=s.substring(i);
+            return s;
+            
+    }
+    private int compare(String s1,String s2){
+            System.out.println(s1+" "+s2);
+            int n1=s1.length();
+            int n2=s2.length();
+            System.out.println(n1+" "+n2);
+            int a=0;
+            int b=0;
+            int temp=n1;
+            int temp1=n2;
+            for(int i=0;i<n1;i++){
+                    a=(int)((s1.charAt(i)-'0')*Math.pow(10,temp-1))+a;
+                    temp--;
+                    
+            }
+            for(int j=0;j<n2;j++){
+                    b=(int)((s2.charAt(j)-'0')*Math.pow(10,temp1-1))+b;
+                    temp1--;
+            }
+            System.out.println(a+" "+b);
+            if(a>b){
+                    return 1;
+            }
+            else if(a<b){
+                    return -1;
+            }
+            return 0;
+            // return false;
+    }
+    public int compareVersion(String s1, String s2) {
+            int n=s1.length();
+            int m=s2.length();
+            s1=s1+'.';
+            s2=s2+'.';
+            int ans=0;
+            while(n>0&&m>0){
+                    int dot1=s1.indexOf('.');
+                    int dot2=s2.indexOf('.');
+                    String str1=removeleadingzero(s1.substring(0,dot1));
+                    String str2=removeleadingzero(s2.substring(0,dot2));
+                    System.out.println(str1+" "+str2);
+                    s1=s1.substring(dot1+1);
+                    s2=s2.substring(dot2+1);
+                    ans=compare(str1,str2);
+                    // System.out.println(ans);
+                    if(ans!=0){
+                            return ans;
+                    }
+                    n=s1.length();
+                    m=s2.length();
+            }
+            // System.out.println(s1+" "+s2);
+            // System.out.println(n+" "+m);
+            while(n!=0){
+                    int dot1=s1.indexOf('.');
+                    String s=s1.substring(0,dot1);
+                    // System.out.println(s);
+                    ans=compare(s,"0");
+                    // System.out.println(ans);
+                    if(ans!=0){
+                            return ans;
+                    }
+                    s1=s1.substring(dot1+1);
+                    n=s1.length();
+            }
+            while(m!=0){
+                    // System.out.println(s2);
+                    int dot1=s2.indexOf('.');
+                    // System.out.println(dot1);
+                    String s=s2.substring(0,dot1);
+                    ans=compare("0",s);
+                    if(ans!=0){
+                            return ans;
+                    }
+                    s2=s2.substring(dot1+1);
+                    m=s2.length();
+                    
+            }
+            return ans;
+            
+            
+    }
+}
