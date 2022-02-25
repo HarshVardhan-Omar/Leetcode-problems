ee510c25003d9140f1a4391c591862a479f906a4
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-02-25 21:40:07 +0530

    Added solution - LeetHub

---
+++ Missing number in array - GFG/missing-number-in-array.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.MissingNumber(array, n));
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int MissingNumber(int arr[], int n) {
+        int i=0;
+        n=n-1;
+        while(i<n){
+            int correct=arr[i]-1;
+            if(correct<n&&arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        i=0;
+        while(i<n){
+            if((i+1)!=arr[i]){
+                return i+1;
+            }
+            i++;
+        }
+        return n+1;
+        
+    }
+}
