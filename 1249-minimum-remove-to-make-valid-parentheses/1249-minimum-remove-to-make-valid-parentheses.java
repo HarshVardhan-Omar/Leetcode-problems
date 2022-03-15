54a1178591903fd4e0d18a9e48d07e4fb9182ffb
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-03-15 16:51:37 +0530

    Time: 825 ms (5.02%), Space: 44 MB (79.66%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+            int n=s.length();
+            Stack<Pair<Character,Integer>> st=new Stack<>();
+            for(int i=0;i<n;i++){
+                    char c=s.charAt(i);
+                    if(c=='('){
+                            st.push(new Pair<>(c,i));
+                    }
+                    else if(c==')'){
+                            if(!st.isEmpty()&&st.peek().getKey()=='('){
+                                    st.pop();
+                            }
+                            else{
+                                    st.push(new Pair<>(c,i));
+                            }
+                    }
+            }
+            String ans="";
+            Set<Integer> indexremove=new HashSet<>();
+            while(!st.isEmpty()){
+                    indexremove.add(st.peek().getValue());
+                    st.pop();
+            }
+            for(int i=0;i<n;i++){
+                    if(!indexremove.contains(i)){
+                            ans+=s.charAt(i);
+                    }
+            }
+            return ans;
+        
+    }
+}
