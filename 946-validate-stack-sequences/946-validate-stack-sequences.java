6d4922dd34528e55191a5383aac6c3c4d83a1a1d
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-03-17 01:27:41 +0530

    Time: 6 ms (21.57%), Space: 44.8 MB (59.14%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -5,31 +5,15 @@ class Solution {
             int i=0;
             int j=0;
             while(i<n&&j<n){
-                    while(i<n&&pushed[i]!=popped[j]){
-                            st.push(pushed[i]);
-                            i++;
-                    }
-                    if(i<n){
-                          st.push(pushed[i]);
-                            i++;
-                    }
-                    
-                    // System.out.println(st);
-                    if(!st.isEmpty()){
+                    st.push(pushed[i]);
+                    while(!st.isEmpty()&&st.peek()==popped[j]){
                             st.pop();
                             j++;
                     }
-                    
-                    while(j<n&&!st.isEmpty()&&popped[j]==st.peek()){
-                            st.pop();
-                            j++;
-                    }
+                    i++;
                              
             }
-            if(st.isEmpty()){
-                    return true;
-            }
-            return false;
+            return st.isEmpty();
         
     }
 }
