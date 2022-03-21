9a3e8720ce7104fae64c996ac0fbb1e186234f13
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-03-22 02:07:16 +0530

    Time: 4 ms (84.93%), Space: 42.8 MB (39.11%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -8,37 +8,13 @@ class Solution {
             for(int i=0;i<n;i++){
                     lastindex[s.charAt(i)-'a']=i;
             }
-            // System.out.println(Arrays.toString(lastindex));
             int prevlast=-1;
             int start=0;
             int end=lastindex[s.charAt(start)-'a'];
-            // System.out.println(end);
             int size=0;
             int temp=start;
-            // visited[s.charAt(0)-'a']=1;
             while(temp<n&&end<n){
-                    if(visited[s.charAt(temp)-'a']==1){
-                            if(temp==end){
-                            // System.out.println("hi");
-                            size=end-start+1;
-                            ans.add(size);
-                            start=temp+1;
-                            if(start<n){
-                            end=lastindex[s.charAt(start)-'a'];
-                            }
-                            temp=start;
-                            size=0;
-                            prevlast=-1;
-                            continue;     
-                            }
-                            temp++;
-                            continue;
-                    }
-                    
-                    visited[s.charAt(temp)-'a']=1;
-                    // System.out.println(temp+" "+end);
                     if(temp==end){
-                            // System.out.println("hi");
                             size=end-start+1;
                             ans.add(size);
                             start=temp+1;
