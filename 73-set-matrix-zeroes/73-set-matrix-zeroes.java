6514a7ae81020e4bcea8c1749da1fb2784ab39b2
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-04-21 23:32:10 +0530

    Time: 1 ms (87.71%), Space: 54.4 MB (28.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,38 +1,29 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+            int col0=1;
             int rows=matrix.length;
             int cols=matrix[0].length;
-            int[][] visited=new int[rows][cols];
+            
             for(int i=0;i<rows;i++){
-                    for(int j=0;j<cols;j++){
-                            visited[i][j]=0;
+                    if(matrix[i][0] == 0 )col0=0;
+                    for(int j=1;j<cols;j++){
+                            if(matrix[i][j] == 0){
+                                    matrix[i][0]=0;
+                                    matrix[0][j]=0;
+                            }
                     }
             }
-            int i=0;
-            while(i<rows){
-                    int j=0;
-                    while(j<cols){
-                            if(matrix[i][j]==0&&visited[i][j]==0){
-                                    for(int row=0;row<rows;row++){
-                                            if(matrix[row][j]==0){
-                                                    continue;
-                                            }
-                                            matrix[row][j]=0;
-                                            visited[row][j]=1;
-                                    }
-                                    for(int col=0;col<cols;col++){
-                                            if(matrix[i][col]==0){
-                                                    continue;
-                                            }
-                                            // System.out.println("inside");
-                                            matrix[i][col]=0;
-                                            visited[i][col]=1;
-                                    }
-                                   
+            
+            for(int i=rows-1;i >= 0;i--){
+                    for(int j=cols-1;j>=1;j--){
+                            if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                                    matrix[i][j]=0;
                             }
-                            j++;
                     }
-                    i++;
+                    if(col0==0){
+                            matrix[i][0]=0;
+                    }
+                    
             }
             
         
