ad47cc5e9e6178358a5cca33df2778e38529fbdc
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-04-24 14:24:58 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (73.96%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -0,0 +1,74 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    private void insertAttail(Node head, Node tail,int data){
+            Node newnode=new Node(data);
+            if(head == null){
+                    head=newnode;
+                    tail=newnode;
+                    return;
+            }
+            else{
+                    tail.next=newnode;
+                    tail=newnode;
+            }
+    }
+    public Node copyRandomList(Node head) {
+            // Step1 : Create normal clone using next pointer 
+             Node temp=head;
+             Node clonehead=null;
+             Node clonetail=null;
+            
+            while(temp != null){
+                    Node newnode=new Node(temp.val);
+                    if(clonehead == null){
+                            clonehead=newnode;
+                            clonetail=newnode;
+                    }
+                    else{
+                            clonetail.next=newnode;
+                            clonetail=newnode;
+                    }
+                    temp=temp.next;
+            }
+            
+            //Step 2: Create a mapping to store random pointers
+            HashMap<Node,Node> mp=new HashMap<>();
+            
+            temp=head;
+            Node clone=clonehead;
+            
+            while(temp != null && clone != null){
+                    mp.put(temp,clone);
+                    temp=temp.next;
+                    clone=clone.next;
+            }
+            
+            // Step3: Make random pointer allocation
+            
+            clone=clonehead;
+            Node originalhead=head;
+            while(clone != null){
+                    clone.random=mp.get(originalhead.random);
+                    clone=clone.next;
+                    originalhead=originalhead.next;
+            }
+            return clonehead;
+            
+            
+        
+    }
+}
