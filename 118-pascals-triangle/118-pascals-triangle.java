3ed3dc2d12631eef2bc2bdea6a5b1f2020a25664
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-04-25 22:09:49 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (6.37%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,48 +1,22 @@
 class Solution {
-   public List<List<Integer>> generate(int numRows) {
-        
-        List<List<Integer>> ans = new ArrayList(); 
-        
-        int[][] dp = new int[numRows][numRows];
-        
-        
-        dp[0][0] = 1 ;
-        
-        List<Integer> temp = new ArrayList();
-        
-        temp.add(1);
-        
-        ans.add(temp);
-
-        
-        for(int i=1 ; i<numRows ; i++){
+    public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> ans=new ArrayList<>();
+            List<Integer> row,pre=null;
             
-             temp = new ArrayList();
-          
-            for(int c=0 ; c<numRows ; c++){
-                    
-                    if(c==0){
-                        
-                       temp.add(1);
-                       dp[i][c] = 1 ;
-                        
+            for(int i=0;i<numRows;i++){
+                    row=new ArrayList<>();
+                    for(int j=0;j<=i;j++){
+                            if(j == 0 || j == i){
+                                    row.add(1);
+                            }
+                            else{
+                                    row.add(pre.get(j-1)+pre.get(j));
+                            }
                     }
-                    else{
-                        
-                       dp[i][c] = dp[i-1][c] + dp[i-1][c-1]; 
-                        
-                       if(dp[i][c]!=0) temp.add(dp[i][c]);
-                        
-                       else break ;
-                        
-                    }    
-                    
-                } 
-            ans.add(temp);
-        }
-        
-        
-        return ans ;
+                    pre=row;
+                    ans.add(row);
+            }
+            return ans;
         
     }
 }
