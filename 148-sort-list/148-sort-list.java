53365f0815efe55a3ecae7eba2277f1de7e7fc0f
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-04-25 12:20:26 +0530

    Time: 7 ms (88.54%), Space: 51.5 MB (80.00%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -9,21 +9,58 @@
  * }
  */
 class Solution {
-public ListNode sortList(ListNode head) {
-   ListNode Fake = new ListNode(-1);
-    ArrayList<Integer> list = new ArrayList();
-    ListNode temp = head;
-    while(temp!=null){
-        list.add(temp.val); 
-        temp=temp.next;
+    private ListNode getmid(ListNode head){
+            ListNode slow=head;
+            ListNode fast=head.next;
+            
+            while(fast != null && fast.next != null){
+                    fast=fast.next.next;
+                    slow=slow.next;
+            }
+            return slow;
     }
-    Collections.sort(list);
-    ListNode FakeHead = Fake;
-    for(int i =0; i<list.size(); i++){
-        FakeHead.next = new ListNode(list.get(i));
-        FakeHead = FakeHead.next;
+    private ListNode merge(ListNode l1, ListNode l2){
+            if(l1 == null){
+                    return l2;
+            }
+            if(l2 == null){
+                    return l1;
+            }
+            if(l1.val>l2.val){
+                    ListNode temp=l1;
+                    l1=l2;
+                    l2=temp;
+            }
+            ListNode res=l1;
+            while(l1 != null && l2 != null){
+                    ListNode temp=null;
+                    while(l1 != null && l1.val <= l2.val){
+                            temp=l1;
+                            l1=l1.next;
+                    }
+                    temp.next=l2;
+                    temp=l1;
+                    l1=l2;
+                    l2=temp;
+            }
+            return res;
+            
     }
-    return Fake.next;
+    public ListNode sortList(ListNode head) {
+            if(head == null || head.next == null){
+                    return head;
+            }
+            
+            //Now we need to find the middle of the linked list
+            ListNode mid=getmid(head);
+            ListNode left=head;
+            ListNode right=mid.next;
+            mid.next=null;
+            left=sortList(left);
+            right=sortList(right);
+            
+            ListNode result=merge(left,right);
+            return result;
+        
+    }
 }
-}
-
