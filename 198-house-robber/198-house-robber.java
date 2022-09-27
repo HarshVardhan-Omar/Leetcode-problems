c31c19d147701547c7bbaba585ab8b2410d72670
Harsh Vardhan omar <harshvardhan181201@gmail.com>
2022-09-27 18:07:58 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (54.57%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,18 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
+        int[] dp=new int[n];
         return findmax(nums,n-1,dp);
     }
     int findmax(int[] nums,int ind,int[] dp){
-            if(ind == 0)return nums[ind];
-            if(ind < 0)return 0;
-            if(dp[ind] != -1)return dp[ind];
-            
-            int pick=nums[ind]+findmax(nums,ind-2,dp);
-            int nonpick=findmax(nums,ind-1,dp);
-            
-            return dp[ind]=Math.max(pick,nonpick);
+            dp[0]=nums[0];
+            for(int i=1;i<nums.length;i++){
+                    int pick=0;
+                    if(i>1){
+                        pick=nums[i]+dp[i-2];
+                    }
+                    else{
+                            pick=nums[i];
+                    }
+                    int nonpick=dp[i-1];
+                    dp[i]=Math.max(pick,nonpick);
+            }
+         
+            return dp[nums.length-1];
     }
 }
